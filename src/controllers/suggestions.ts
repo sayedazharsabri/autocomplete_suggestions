149596969bb13d4aa687da815c8e55b59f0358d2
Sayed Azhar Sabri <afzal.sayed26@gmail.com>
2022-03-27 20:55:29 +0200

    Added basic functionality and query to fetch data for given values

--- src/controllers/suggestions.ts
+++ src/controllers/suggestions.ts
@@ -2,9 +2,43 @@ import { RequestHandler } from "express";
 import { IRequest } from '../interface/IRequest';
 import { City } from '../models/city';
 
-export const searchCities: RequestHandler = (req, res) => {
+export const searchCities: RequestHandler = async (req, res) => {
 
-    const reqQuery: IRequest = req.query as any;
-    const city = new City();
-    res.status(200).json({ status: 'success' })
+    try {
+        const reqQuery: IRequest = req.query as any;
+        const q: string = reqQuery.q;
+        const longitude: number = parseFloat(reqQuery.longitude);
+        const latitude: number = parseFloat(reqQuery.latitude);
+
+        let radius: number;
+        if (!!reqQuery.radius) {
+            // Converting radius into meters
+            radius = parseFloat(reqQuery.radius) * 1000;
+        }
+
+        let sort: string;
+        if (!!reqQuery.sort) {
+            sort = reqQuery.sort;
+        }
+
+        // Regular expression to match partial or complete name
+        const regex = new RegExp(`^${q}`);
+
+        const result = await City.aggregate(
+            [
+                {
+                    $geoNear: {
+                        near: { type: "Point", coordinates: [longitude, latitude] },
+                        distanceField: "distance"
+                    }
+                },
+                { $addFields: { results: { $regexMatch: { input: "$name", regex } } } },
+                { $match: { results: true } }
+            ]
+        )
+
+        res.status(200).json({ status: 'success', data: result });
+    } catch (error: any) {
+        res.status(500).send(error.message);
+    }
 }
