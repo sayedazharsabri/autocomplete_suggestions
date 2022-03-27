7f41194b933e48d4d5c57dac8e4e87adc13f167a
Sayed Azhar Sabri <afzal.sayed26@gmail.com>
2022-03-27 18:27:10 +0200

    Added model on controller and removed console.log

--- src/controllers/suggestions.ts
+++ src/controllers/suggestions.ts
@@ -1,10 +1,10 @@
 import { RequestHandler } from "express";
 import { IRequest } from '../interface/IRequest';
+import { City } from '../models/city';
 
 export const searchCities: RequestHandler = (req, res) => {
 
     const reqQuery: IRequest = req.query as any;
-    console.log(reqQuery);
-
+    const city = new City();
     res.status(200).json({ status: 'success' })
 }
