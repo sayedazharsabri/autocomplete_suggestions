5279b5802544d2e2bd75b41dfbdf3ebec7a8f0a0
Sayed Azhar Sabri <afzal.sayed26@gmail.com>
2022-03-28 14:56:48 +0200

    Added integration test to verify success response with data

--- test/integration/suggestions.int.spec.ts
+++ test/integration/suggestions.int.spec.ts
@@ -1,6 +1,10 @@
 import app from "../../src/app";
 import request from "supertest";
 
+import { City } from '../../src/models/city';
+import cityData from '../mockdata/cities.json'
+import console from "console";
+
 describe("Test to suggestions APIs", () => {
     it("GET / should return status 200 with valid message", async () => {
         const result = await request(app).get("/");
@@ -8,4 +12,11 @@ describe("Test to suggestions APIs", () => {
         expect(result.statusCode).toEqual(200);
     });
 
+    it("should return status 200 with data GET /suggestions with query parameters", async () => {
+        const apiWithQuery = "/suggestions?q=Acton&latitude=34.46999&longitude=-122.25257&radius=393116.845&sort=distance";
+        City.aggregate = jest.fn(() => Promise.resolve(cityData) as any);
+        const result = await request(app).get(apiWithQuery);
+        expect(result.statusCode).toEqual(200);
+        expect(JSON.parse(result.text).suggestions).toStrictEqual(cityData);
+    });
 });
